dda08dfd776c34234e2b7a325daa5349f8022d58
Matthias Noback <matthiasnoback@gmail.com>
2017-05-06 09:30:46 +0200

    Modernize and document EventDispatcher

--- src/Common/EventDispatcher/EventDispatcher.php
+++ src/Common/EventDispatcher/EventDispatcher.php
@@ -10,17 +10,33 @@ final class EventDispatcher
     private $subscribers = [];
     private $subscribedToAllEvents = [];
 
-    public function registerSubscriber(string $eventName, callable $subscriber)
+    /**
+     * Subscribe to a single type of event that is dispatched through this event dispatcher, based on its full class name.
+     *
+     * @param string $eventClassName Only subscribe to events of this class
+     * @param callable $subscriber Any callable accepting instances of the corresponding event class as its first argument
+     */
+    public function registerSubscriber(string $eventClassName, callable $subscriber): void
     {
-        $this->subscribers[$eventName][] = $subscriber;
+        $this->subscribers[$eventClassName][] = $subscriber;
     }
 
-    public function subscribeToAllEvents(callable $subscriber)
+    /**
+     * Subscribe to any event that is dispatched through this event dispatcher.
+     *
+     * @param callable $subscriber Any callable accepting untyped event objects as its first argument
+     */
+    public function subscribeToAllEvents(callable $subscriber): void
     {
         $this->subscribedToAllEvents[] = $subscriber;
     }
 
-    public function dispatch($event)
+    /**
+     * Dispatch a single event.
+     *
+     * @param object $event The event object
+     */
+    public function dispatch($event): void
     {
         Assertion::isObject($event, 'An event should be an object');
 
@@ -35,6 +51,11 @@ final class EventDispatcher
         }
     }
 
+    /**
+     * Dispatch all the given events.
+     *
+     * @param object[]|array $events An array of event objects
+     */
     public function dispatchAll(array $events): void
     {
         foreach ($events as $event) {
