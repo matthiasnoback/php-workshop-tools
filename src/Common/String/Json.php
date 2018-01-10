61431ba7ee560d4d09e3b10f841aee7f36a5c475
Matthias Noback <matthiasnoback@gmail.com>
2018-01-10 19:41:49 +0100

    Add simple JSON encode/decode function

---
+++ src/Common/String/Json.php
@@ -0,0 +1,38 @@
+<?php
+declare(strict_types=1);
+
+namespace Common\String;
+
+final class Json
+{
+    /**
+     * @param mixed $data
+     * @return string
+     * @throws \InvalidArgumentException
+     */
+    public static function encode($data): string
+    {
+        $result = json_encode($data);
+
+        if ($result === false && json_last_error()) {
+            throw new \InvalidArgumentException('JSON encoding error: ' . json_last_error_msg());
+        }
+
+        return $result;
+    }
+
+    /**
+     * @param string $string
+     * @return mixed
+     */
+    public static function decode(string $string)
+    {
+        $result = json_decode($string);
+
+        if ($result === null && json_last_error()) {
+            throw new \InvalidArgumentException('JSON decoding error: ' . json_last_error_msg());
+        }
+
+        return $result;
+    }
+}
---
+++ test/Integration/Common/String/JsonTest.php
@@ -0,0 +1,46 @@
+<?php
+declare(strict_types=1);
+
+namespace Test\Integration\Common\String;
+
+use Common\String\Json;
+use PHPUnit\Framework\TestCase;
+
+final class JsonTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function it_encodes_data()
+    {
+        self::assertEquals('{"foo":"bar"}', Json::encode(['foo' => 'bar']));
+    }
+
+    /**
+     * @test
+     */
+    public function it_decodes_data_and_returns_objects_instead_of_arrays()
+    {
+        self::assertEquals((object)['foo' => 'bar'], Json::decode('{"foo":"bar"}'));
+    }
+
+    /**
+     * @test
+     */
+    public function it_throws_an_exception_upon_an_encoding_error()
+    {
+        $canNotEncodeThis = fopen(__FILE__, 'r+b');
+
+        $this->expectException(\InvalidArgumentException::class);
+        Json::encode($canNotEncodeThis);
+    }
+
+    /**
+     * @test
+     */
+    public function it_throws_an_exception_upon_a_decoding_error()
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        Json::decode('{"invalid');
+    }
+}
