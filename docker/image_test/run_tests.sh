e7a66e10dd8d1aebefb144a88dfaabc9ffacb53e
Matthias Noback <matthiasnoback@gmail.com>
2018-01-10 17:23:18 +0100

    Back to port 8080

--- docker/image_test/run_tests.sh
+++ docker/image_test/run_tests.sh
@@ -10,14 +10,14 @@ function failure {
     exit 1
 }
 
-output=$(curl http://simple_webserver/ 2> /dev/null)
+output=$(curl http://simple_webserver:8080/ 2> /dev/null)
 if [ "$output" != "It works!" ]; then
     failure "simple_webserver didn't serve a PHP file"
 else
     success "simple_webserver serves a PHP file"
 fi
 
-output=$(curl http://simple_webserver/static.html 2> /dev/null)
+output=$(curl http://simple_webserver:8080/static.html 2> /dev/null)
 if [ "$output" != "Static file" ]; then
     failure "simple_webserver didn't serve a static HTML file"
 else
