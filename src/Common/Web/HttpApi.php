d6e1d19271a2ad1df3ee88cde5e9dedf69868f50
Matthias Noback <matthiasnoback@gmail.com>
2018-01-10 20:18:22 +0100

    Add an HttpApi facade

---
+++ src/Common/Web/HttpApi.php
@@ -0,0 +1,38 @@
+<?php
+declare(strict_types=1);
+
+namespace Common\Web;
+
+use Common\String\Json;
+
+final class HttpApi
+{
+    /**
+     * Fetch a response and JSON-decode it in one go.
+     *
+     * @param string $url
+     * @return mixed
+     */
+    public static function fetchDecodedJsonResponse(string $url)
+    {
+        return Json::decode(self::fetchResponse($url));
+    }
+
+    /**
+     * Fetch the HTTP response for a given URL (makes a GET request).
+     *
+     * @param string $url
+     * @return string
+     */
+    public static function fetchResponse(string $url): string
+    {
+        $context = stream_context_create(['http' => ['timeout' => 5]]);
+        $response = file_get_contents($url, false, $context);
+
+        if ($response === false) {
+            throw new \RuntimeException('Failed to make a request to: ' . $url);
+        }
+
+        return $response;
+    }
+}
